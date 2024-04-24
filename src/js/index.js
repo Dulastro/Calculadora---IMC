f4161a0e19005a63d2979edad6de9e236ff88db7
Pedro Dulastro <98427103+Dulastro@users.noreply.github.com>
2024-04-24 15:08:23 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,31 @@
 let inputs = document.querySelectorAll("input");
 let btn = document.querySelector("button");
 
+bntDarnkMode.addEventListener("click", () => {
+  let form = document.querySelector("form")
+  form.classList.toggle("dark-mode");
+});
+
 btn.addEventListener("click", (ev) => {
   ev.preventDefault();
-  let inputValues = getValueInput(); // Obtenha os valores dos inputs
-  let imc = calculeteImc(inputValues[0], inputValues[1]); // Calcule o IMC
-  showImc(imc); // Mostre o IMC
+  let inputValues = getValueInput();
+  let imc = calculeteImc(inputValues[0], inputValues[1]);
+  showImc(imc);
+  removeBwColor(imc);
 });
 
 function getValueInput() {
-  let inputValues = []; // Crie um novo array para armazenar os valores
+  let inputValues = [];
   inputs.forEach((input) => {
-    inputValues.push(parseFloat(input.value)); // Converta para número (float)
-    input.value = ""; // Limpe os valores dos inputs
+    inputValues.push(parseFloat(input.value));
+    input.value = "";
   });
-  return inputValues; // Retorne os valores dos inputs
+  return inputValues;
 }
 
 function calculeteImc(peso, altura) {
-  let imc = peso / (altura * altura); // Fórmula correta para calcular IMC
-  return imc; // Retorne o valor do IMC
+  let imc = peso / (altura * altura);
+  return imc;
 }
 
 function showImc(imc) {
@@ -29,8 +35,29 @@ function showImc(imc) {
   span.className = "span-imc";
   let p = document.createElement("p");
   p.className = "imcshow";
-  p.innerHTML = "Resultado: " + imc.toFixed(2); // Arredonde o IMC para 2 casas decimais
+  p.innerHTML = "Resultado: " + imc.toFixed(2);
 
   form.appendChild(span);
   span.appendChild(p);
+}
+
+function removeBwColor(imc) {
+  let img = document.querySelectorAll("img");
+
+  if (imc >= 0 && imc < 18.5) {
+    // Abaixo do peso
+    img[0].classList.add("bw-color");
+  } else if (imc >= 18.5 && imc < 25) {
+    // Peso normal
+    img[1].classList.add("bw-color");
+  } else if (imc >= 25 && imc < 30) {
+    // Sobrepeso
+    img[2].classList.add("bw-color");
+  } else if (imc >= 30 && imc < 40) {
+    // Obesidade
+    img[3].classList.add("bw-color");
+  } else if (imc >= 40) {
+    // Obesidade mórbida
+    img[4].classList.add("bw-color");
+  }
 }
